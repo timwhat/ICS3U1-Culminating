698211a80e739efa894e6924b3a309035ea8b9bc
Timothy L <110949742+timwhat@users.noreply.github.com>
2025-01-10 01:07:17 -0500

    I GOT THE TABLE WORKING

--- src/main.py
+++ src/main.py
@@ -3,17 +3,6 @@ from prettytable import PrettyTable
 
 from utils import *
 
-# Config
-saveGameFile = 'save/save.txt'
-playersDataFile = 'save/playerData.txt'
-
-# Global Variables
-playersData = []
-gamesData = []
-
-usernameRegex = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_-")
-letterLegend = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
-
 def main():
 
     global playersData
@@ -71,7 +60,7 @@ def main():
                     print("what action do you want to do?")
                     print("1. reveal a position")
                     print("2. try to guess the amounty to guess the amount")
-                    decision =  int(input)
+                    decision =  int(input())
                     if decision == 1:
                         print("where do you want to check? (ex. b2, h6, etc.)")
                         move = str(input())
@@ -113,159 +102,5 @@ def main():
             elif choice == 5:
                 DuckPlayer.savePlayerData()
 
-def textSeperator():
-    print('\n')
-    print('/************************************************************************/')
-    print('\n')
-
-class Game: # player to keep track of whos board it is
-    def __init__(self, size, moves, player, numducks):
-        self.size = size
-        self.board = []
-        self.moves = moves
-        self.player = player
-        self.numducks = numducks
-
-    #TODO reverse the x and y of board to print that with the add_rows
-    # then add logic for what is revealed
-    def printBoard(self):
-        table = PrettyTable()
-        tmpLegend = []
-        tmpNumLegend = []
-        # Adding the legend
-        for i in range(self.size):
-            tmpLegend.append(letterLegend[i])
-            tmpNumLegend.append(i+1)
-        # table.add_column([""] + tmp_legend)
-                
-        table.add_column("", tmpNumLegend)
-        for i in range(self.size):
-            # tmp.extend(self.board[i])
-            table.add_column(str(i+1), self.board[i])
-        print(table)
-
-    def generateboard(self):
-        self.board = []
-        tempCoords = []
-        for x in range(self.size):
-            column = []
-            for y in range(self.size):
-                column.append([False,"a",False])
-                tempCoords.append([x,y])
-            self.board.append(column)
-
-        for i in range(self.numducks):
-            coords = random.choice(tempCoords)
-            tempCoords.remove(coords)
-            # print(coords,"was determined to be a duck")
-            self.board[coords[0]][coords[1]][0] = True
-
-        for y in range(self.size):
-            temprow = ""
-            for x in range(self.size):
-                if self.board[x][y][0]:
-                    temprow = temprow + "1"
-                elif not self.board[x][y][0]:
-                    temprow = temprow + "0"
-
-        for x in range(self.size):
-            for y in range(self.size):
-                tempduckcount = 0
-                surroundings = giveSurroundings(x,y,self)
-                for row in surroundings:
-                    for tile in row:
-                        if tile:
-                            tempduckcount += 1
-                self.board[x][y][1] = tempduckcount
-
-    def reveal(self, pos):
-        y = int(pos[1])-1
-        x = letterLegend.index(pos[0])
-        print(x,y)
-
-    
-    def guess(self,guess):
-        if guess == self.numducks:
-            print("correct!")
-
-    # def saveGame(self):
-    #     # TODO: Save the game to a file    
-
-class DuckPlayer:
-    def __init__(self, name, gamesWon, gamesLost, winStreak):
-        self.name = name
-        self.gamesWon = gamesWon
-        self.gamesLost = gamesLost
-        self.winStreak = winStreak
-
-    def loadplayer():
-        global playersData
-        playerloaded = False
-        
-        while not playerloaded:
-            name = str((str(input("enter name:\t"))).lower())
-            # REGEX CHECK
-            if not all((c in usernameRegex) for c in name):
-                print("Invalid Name")
-                continue
-            # print("name:",name)
-            alreadyexists = False
-            for i in playersData:
-                savedPlayer = i
-                if i.name == name:
-                    alreadyexists = True
-                    break
-            if alreadyexists:
-                decision = ''
-                while decision not in ["y","n"]:
-                    decision = (str(input(("There is already a player with the name: " + name + ", Would you like to continue as this person? (y/n)")))).lower()
-                if decision == 'y':
-                    player = savedPlayer
-                    playerloaded = True
-                elif decision == "n":
-                    continue
-            elif not alreadyexists:
-                decision = ''
-                while decision not in ["y","n"]:
-                    decision = (str(input(("want to make a new profile as " + name + "? (y/n)")))).lower()
-                if decision == 'y':
-                    player = DuckPlayer(name,0,0,0)
-                    playersData.append(player)
-                    playerloaded = True
-                elif decision == "n":
-                    continue
-        return player
-
-    def loadSavedFiles():
-        global playersData
-        global gamesData
-
-        with open(playersDataFile, "r") as file:
-            tmpPlayersData = file.read().strip().split("\n")
-        with open(saveGameFile, "r") as file:
-            gamesData = file.read().strip().split("\n")
-
-        for playerData in tmpPlayersData:
-            if playerData:
-                data = playerData.split(",")
-                playersData.append(DuckPlayer(data[0], int(data[1]), int(data[2]), int(data[3])))
-
-    def savePlayerData():
-        with open(playersDataFile,"w") as file:
-            for i in playersData:
-                file.write(i.name + "," + str(i.gamesWon) + "," + str(i.gamesLost) + "," + str(i.winStreak) + "\n")
-                
-def giveSurroundings(startx,starty,game):
-    mainList = [[],[],[]]
-    for y in range(-1,2):
-        for x in range(-1,2):
-            mainList[y+1].append(getduck(startx+x,starty+y,game))
-    return(mainList)
-
-def getduck(x,y,game):
-    if (0 <= x <= game.size-1) and (0 <= y <= game.size-1):
-        return game.board[x][y][0]
-    else:
-        return False
 
 main()
---
+++ src/utils.py
@@ -0,0 +1,180 @@
+import random
+from prettytable import PrettyTable
+
+# Global Variables
+playersData = []
+gamesData = []
+
+# Config
+saveGameFile = 'save/save.txt'
+playersDataFile = 'save/playerData.txt'
+
+usernameRegex = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_-")
+letterLegend = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+def textSeperator():
+    print('\n')
+    print('/************************************************************************/')
+    print('\n')
+
+class Game: # player to keep track of whos board it is
+    def __init__(self, size, moves, player, numducks):
+        self.size = size
+        self.board = []
+        self.moves = moves
+        self.player = player
+        self.numducks = numducks
+
+    #TODO make this generate board and have it return the thing to print
+    # then add logic for what is revealed
+    def printBoard(self):
+        table = PrettyTable()
+
+        tmpLetterLegend = []
+        tmpNumLegend = []
+
+        # Adding the legend
+        for i in range(self.size):
+            tmpLetterLegend.append(letterLegend[i])
+            tmpNumLegend.append(i+1)
+        table.field_names = [""] + tmpLetterLegend
+                
+        # Adding the rows
+        for i in range(self.size):
+            # Chosing whcih items to display
+            showedRow = []
+            for j in range(self.size):
+                if self.board[j][i][2]:
+                    if self.board[j][i][0]:
+                        showedRow.append("D")
+                    else:
+                        showedRow.append(str(self.board[j][i][1]))
+                else:
+                    showedRow.append("-")
+
+            table.add_row([str(tmpNumLegend[i])] + showedRow, divider=True)
+        print(table)
+
+    def generateboard(self):
+        self.board = []
+        tempCoords = []
+        for x in range(self.size):
+            column = []
+            for y in range(self.size):
+                column.append([False,"a",False])
+                tempCoords.append([x,y])
+            self.board.append(column)
+
+        for i in range(self.numducks):
+            coords = random.choice(tempCoords)
+            tempCoords.remove(coords)
+            # print(coords,"was determined to be a duck")
+            self.board[coords[0]][coords[1]][0] = True
+
+        for y in range(self.size):
+            temprow = ""
+            for x in range(self.size):
+                if self.board[x][y][0]:
+                    temprow = temprow + "1"
+                elif not self.board[x][y][0]:
+                    temprow = temprow + "0"
+
+        for x in range(self.size):
+            for y in range(self.size):
+                tempduckcount = 0
+                surroundings = giveSurroundings(x,y,self)
+                for row in surroundings:
+                    for tile in row:
+                        if tile:
+                            tempduckcount += 1
+                self.board[x][y][1] = tempduckcount
+
+    def reveal(self, pos):
+        y = int(pos[1])-1
+        x = letterLegend.index(pos[0])
+        print(x,y)
+
+    
+    def guess(self,guess):
+        if guess == self.numducks:
+            print("correct!")
+
+    # def saveGame(self):
+    #     # TODO: Save the game to a file    
+
+class DuckPlayer:
+    def __init__(self, name, gamesWon, gamesLost, winStreak):
+        self.name = name
+        self.gamesWon = gamesWon
+        self.gamesLost = gamesLost
+        self.winStreak = winStreak
+
+    def loadplayer():
+        global playersData
+        playerloaded = False
+        
+        while not playerloaded:
+            name = str((str(input("enter name:\t"))).lower())
+            # REGEX CHECK
+            if not all((c in usernameRegex) for c in name):
+                print("Invalid Name")
+                continue
+            # print("name:",name)
+            alreadyexists = False
+            for i in playersData:
+                savedPlayer = i
+                if i.name == name:
+                    alreadyexists = True
+                    break
+            if alreadyexists:
+                decision = ''
+                while decision not in ["y","n"]:
+                    decision = (str(input(("There is already a player with the name: " + name + ", Would you like to continue as this person? (y/n)")))).lower()
+                if decision == 'y':
+                    player = savedPlayer
+                    playerloaded = True
+                elif decision == "n":
+                    continue
+            elif not alreadyexists:
+                decision = ''
+                while decision not in ["y","n"]:
+                    decision = (str(input(("want to make a new profile as " + name + "? (y/n)")))).lower()
+                if decision == 'y':
+                    player = DuckPlayer(name,0,0,0)
+                    playersData.append(player)
+                    playerloaded = True
+                elif decision == "n":
+                    continue
+        return player
+
+    def loadSavedFiles():
+        global playersData
+        global gamesData
+
+        with open(playersDataFile, "r") as file:
+            tmpPlayersData = file.read().strip().split("\n")
+        with open(saveGameFile, "r") as file:
+            gamesData = file.read().strip().split("\n")
+
+        for playerData in tmpPlayersData:
+            if playerData:
+                data = playerData.split(",")
+                playersData.append(DuckPlayer(data[0], int(data[1]), int(data[2]), int(data[3])))
+
+    def savePlayerData():
+        with open(playersDataFile,"w") as file:
+            for i in playersData:
+                file.write(i.name + "," + str(i.gamesWon) + "," + str(i.gamesLost) + "," + str(i.winStreak) + "\n")
+                
+def giveSurroundings(startx,starty,game):
+    mainList = [[],[],[]]
+    for y in range(-1,2):
+        for x in range(-1,2):
+            mainList[y+1].append(getduck(startx+x,starty+y,game))
+    return(mainList)
+
+def getduck(x,y,game):
+    if (0 <= x <= game.size-1) and (0 <= y <= game.size-1):
+        return game.board[x][y][0]
+    else:
+        return False
